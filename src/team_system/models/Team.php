81b4bded76e78835716ce9d4311428843827ed92
teiylog <55331682+teiylog@users.noreply.github.com>
2019-10-30 16:19:04 +0900

    コード整形してやったぜ。フン！

--- src/team_system/models/Team.php
+++ src/team_system/models/Team.php
@@ -4,7 +4,6 @@ namespace team_system\models;
 
 class Team
 {
-
     private $id;
     private $owner;
     private $members;
@@ -12,25 +11,22 @@ class Team
     /**
      * @return string
      */
-    public function getId(): string
-    {
+    public function getId(): string {
         return $this->id;
     }
 
     /**
      * @return Player
      */
-    public function getOwner(): Player
-    {
+    public function getOwner(): Player {
         return $this->owner;
     }
 
-    public function join(){
+    public function join() {
         //TODO:参加
     }
 
-    public function __construct(Player $owner)
-    {
+    public function __construct(Player $owner) {
         $this->id = uniqid();
         $this->owner = $owner;
     }
