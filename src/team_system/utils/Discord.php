12369b400ce2a01fb4a704717c2acb1e0f0a437d
teiylog <55331682+teiylog@users.noreply.github.com>
2019-11-01 17:52:16 +0900

    utils\Discord.phpとtasks\DiscordAsyncTask.phpをついかしちゃった、えへへ

---
+++ src/team_system/utils/Discord.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace team_system\utils;
+
+use pocketmine\Server;
+use team_system\tasks\DiscordAsyncTask;
+
+class Discord {
+
+	private $name;
+	private $webhook;
+
+	public function __construct( string $webhook, string $name = "Server" ) {
+		$this->name = $name;
+		$this->webhook = $webhook;
+	}
+
+	public function getName(): string {
+		return $this->name;
+	}
+
+	public function setName( string $name ): void {
+		$this->name = $name;
+	}
+
+	public function sendMessage( string $message ): void {
+        $curlopts = [ 'content' => $message, 'username' => $this->name ];
+        Server::getInstance()->getAsyncPool()->submitTask( new DiscordAsyncTask( $this->webhook, serialize( $curlopts ) ));
+    }
+}
