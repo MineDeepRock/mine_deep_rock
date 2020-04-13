6577bf9cf21617722de68b38f7b625030bb8970c
suinua <maatinosippo@gmail.com>
2020-04-13 22:31:48 +0900

    モデリング

--- src/main_system/models/Game.php
+++ src/main_system/models/Game.php
@@ -7,4 +7,8 @@ namespace main_system\models;
 class Game
 {
     private $isStarted;
+
+    private $firstMap;
+    private $secondMap;
+    private $thirdMap;
 }
---
+++ src/main_system/service/GameService.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace main_system\service;
+
+
+class GameService
+{
+    
+    //TODO:
+   public function GameStart(string $teamId,array $players){
+
+   }
+
+   //TODO:
+   private function selectMap(){
+
+   }
+}
