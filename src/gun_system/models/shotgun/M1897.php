5d2d729fce139afde336b1ec4cadb74bd8f249f8
suinua <maatinosippo@gmail.com>
2020-04-17 23:44:12 +0900

    バランス調整

--- src/gun_system/models/shotgun/M1897.php
+++ src/gun_system/models/shotgun/M1897.php
@@ -13,6 +13,6 @@ use pocketmine\scheduler\TaskScheduler;
 class M1897 extends Shotgun
 {
     public function __construct(TaskScheduler $scheduler) {
-        parent::__construct(12,8.4, new GunRate(2.3), new BulletSpeed(45), 5, 7.5, new ReloadDuration(2), 12, new GunPrecision(90), $scheduler);
+        parent::__construct(12,8.4, new GunRate(1), new BulletSpeed(45), 5, 7.5, new ReloadDuration(2), 12, new GunPrecision(80), $scheduler);
     }
 }
