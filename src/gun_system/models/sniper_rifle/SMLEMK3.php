902adc7a50bf92635ccba8ef89504297151ed114
suinua <maatinosippo@gmail.com>
2020-04-19 14:04:46 +0900

    SMLEMK3のマガジンキャパを5から10に

--- src/gun_system/models/sniper_rifle/SMLEMK3.php
+++ src/gun_system/models/sniper_rifle/SMLEMK3.php
@@ -13,7 +13,7 @@ use pocketmine\scheduler\TaskScheduler;
 class SMLEMK3 extends SniperRifle
 {
     public function __construct(TaskScheduler $scheduler) {
-        parent::__construct(100, new GunRate(0.8), new BulletSpeed(74), 5, 2.5, new ReloadDuration(1.7), 55, new GunPrecision(98.5), $scheduler);
+        parent::__construct(100, new GunRate(0.8), new BulletSpeed(74), 10, 2.5, new ReloadDuration(1.7), 55, new GunPrecision(98.5), $scheduler);
     }
 
 }
