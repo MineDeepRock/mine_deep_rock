b3684453cc6ca8f5d286d8d873ddeac7e786fcd2
suinua <maatinosippo@gmail.com>
2020-04-24 15:21:55 +0900

    Gewehr98,MartiniHenry,Type38Arisakaを追加
    #8

---
+++ src/gun_system/models/sniper_rifle/Gewehr98.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace gun_system\models\sniper_rifle;
+
+
+use gun_system\models\BulletDamage;
+use gun_system\models\BulletSpeed;
+use gun_system\models\ClipReloadController;
+use gun_system\models\EffectiveRange;
+use gun_system\models\GunPrecision;
+use gun_system\models\GunRate;
+use gun_system\models\OneByOneReloadController;
+use pocketmine\scheduler\TaskScheduler;
+
+class Gewehr98 extends SniperRifle
+{
+    public function __construct(TaskScheduler $scheduler) {
+        parent::__construct(new BulletDamage(100,80), new GunRate(0.8), new BulletSpeed(880),  2.5, new ClipReloadController(5,5,1.5,0.5), new EffectiveRange(30,100), new GunPrecision(99.5,80), $scheduler);
+    }
+}
---
+++ src/gun_system/models/sniper_rifle/Type38Arisaka.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace gun_system\models\sniper_rifle;
+
+
+use gun_system\models\BulletDamage;
+use gun_system\models\BulletSpeed;
+use gun_system\models\ClipReloadController;
+use gun_system\models\EffectiveRange;
+use gun_system\models\GunPrecision;
+use gun_system\models\GunRate;
+use pocketmine\scheduler\TaskScheduler;
+
+class Type38Arisaka extends SniperRifle
+{
+    public function __construct(TaskScheduler $scheduler) {
+        parent::__construct(new BulletDamage(100,70), new GunRate(0.95), new BulletSpeed(770),  2.5, new ClipReloadController(5,5,0.7,0.7), new EffectiveRange(10,30), new GunPrecision(99.5,80), $scheduler);
+    }
+}
