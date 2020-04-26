fdf4eea91749f633b73809a373f710d72d15678d
suinua <maatinosippo@gmail.com>
2020-04-26 19:33:46 +0900

    Model1900のリロードを修正

--- src/gun_system/models/shotgun/Model1900.php
+++ src/gun_system/models/shotgun/Model1900.php
@@ -7,15 +7,15 @@ namespace gun_system\models\shotgun;
 use gun_system\models\attachment\bullet\ShotgunBulletType;
 use gun_system\models\BulletDamage;
 use gun_system\models\BulletSpeed;
+use gun_system\models\ClipReloadController;
 use gun_system\models\EffectiveRange;
 use gun_system\models\GunPrecision;
 use gun_system\models\GunRate;
-use gun_system\models\OneByOneReloadController;
 use pocketmine\scheduler\TaskScheduler;
 
 class Model1900 extends Shotgun
 {
     public function __construct(ShotgunBulletType $bulletType, TaskScheduler $scheduler) {
-        parent::__construct($bulletType, 12, new BulletDamage(12.5, 6.25), new GunRate(5), new BulletSpeed(333),  1, new OneByOneReloadController(2,2.5), new EffectiveRange(0, 13), new GunPrecision(80,76), $scheduler);
+        parent::__construct($bulletType, 12, new BulletDamage(12.5, 6.25), new GunRate(5), new BulletSpeed(333),  1, new ClipReloadController(2,2,2.4,3.2), new EffectiveRange(0, 13), new GunPrecision(80,76), $scheduler);
     }
 }
