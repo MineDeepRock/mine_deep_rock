cc7c5142f1b797c4f3e26cd6f014e308bed3372e
suinua <maatinosippo@gmail.com>
2020-04-27 11:16:39 +0900

    銃弾のパーティクルを大きく

--- src/gun_system/pmmp/entity/EntityBullet.php
+++ src/gun_system/pmmp/entity/EntityBullet.php
@@ -57,7 +57,7 @@ class EntityBullet
                         $projectile->getX(),
                         $projectile->getY(),
                         $projectile->getZ()
-                    ),1));
+                    ),4));
                 }
             }
         ), 1);
