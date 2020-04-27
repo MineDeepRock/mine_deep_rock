5812f340727ea7e005f71b18bc1ddc4cfa93b446
suinua <maatinosippo@gmail.com>
2020-04-27 19:50:50 +0900

    プレイヤーが銃を撃ちながら鯖を抜けたときに、落ちないように

--- src/gun_system/pmmp/entity/Egg.php
+++ src/gun_system/pmmp/entity/Egg.php
@@ -35,20 +35,22 @@ class Egg extends Throwable
         $players = Server::getInstance()->getOnlinePlayers();
 
         foreach ($players as $player) {
-            $distance = $blockPos->distance($player->getPosition());
-            if ($distance <= 5) {
-                GunSounds::play($player, GunSounds::bulletHitBlock());
-                $item = $player->getInventory()->getItemInHand();
-                if (!($player->getName() === $this->getOwningEntity()->getName())) {
-                    if (is_subclass_of($item, "gun_system\pmmp\items\ItemGun")) {
-                        if (!($item instanceof ItemShotGun)) {
-                            $player->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), 20 * 3, 1));
-                            $item->scare();
+            if ($player !== null || $this->getOwningEntity() !== null) {
+                $distance = $blockPos->distance($player->getPosition());
+                if ($distance <= 5) {
+                    GunSounds::play($player, GunSounds::bulletHitBlock());
+                    $item = $player->getInventory()->getItemInHand();
+                    if (!($player->getName() === $this->getOwningEntity()->getName())) {
+                        if (is_subclass_of($item, "gun_system\pmmp\items\ItemGun")) {
+                            if (!($item instanceof ItemShotGun)) {
+                                $player->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), 20 * 3, 1));
+                                $item->scare();
+                            }
                         }
                     }
+                } else if ($distance <= 10) {
+                    GunSounds::play($player, GunSounds::bulletFly());
                 }
-            } else if ($distance <= 10) {
-                GunSounds::play($player, GunSounds::bulletFly());
             }
         }
 
