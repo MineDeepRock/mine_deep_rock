fa8bb2ec3db179218601fbd9ae9de86d2f3b567c
suinua <maatinosippo@gmail.com>
2020-05-19 18:28:00 +0900

    LaveParticleからEntityFlameParticleへ

--- src/game_system/pmmp/client/FlameBottleClient.php
+++ src/game_system/pmmp/client/FlameBottleClient.php
@@ -5,13 +5,13 @@ namespace game_system\pmmp\client;
 
 
 use pocketmine\level\Level;
-use pocketmine\level\particle\LavaParticle;
+use pocketmine\level\particle\EntityFlameParticle;
 use pocketmine\math\Vector3;
 
 class FlameBottleClient
 {
     public function explodeParticle(Level $level, Vector3 $pos): void {
-        $level->addParticle(new LavaParticle($pos));
+        $level->addParticle(new EntityFlameParticle($pos));
     }
 
     public function playSound(Level $level, Vector3 $pos): void {
