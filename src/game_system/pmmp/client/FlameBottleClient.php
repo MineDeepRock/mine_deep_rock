9b40d678eb16f1abec4787fb73c1330f851956d3
suinua <maatinosippo@gmail.com>
2020-05-19 18:21:49 +0900

    火炎瓶のエフェクトをFlameからlaveに

--- src/game_system/pmmp/client/FlameBottleClient.php
+++ src/game_system/pmmp/client/FlameBottleClient.php
@@ -5,13 +5,13 @@ namespace game_system\pmmp\client;
 
 
 use pocketmine\level\Level;
-use pocketmine\level\particle\FlameParticle;
+use pocketmine\level\particle\LavaParticle;
 use pocketmine\math\Vector3;
 
 class FlameBottleClient
 {
     public function explodeParticle(Level $level, Vector3 $pos): void {
-        $level->addParticle(new FlameParticle($pos));
+        $level->addParticle(new LavaParticle($pos));
     }
 
     public function playSound(Level $level, Vector3 $pos): void {
