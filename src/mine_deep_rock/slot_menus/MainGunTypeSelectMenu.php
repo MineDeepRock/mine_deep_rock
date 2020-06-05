ac225aecc479956875147666594c43743e3d4865
suinua <maatinosippo@gmail.com>
2020-06-05 22:49:07 +0900

    fix

--- src/mine_deep_rock/slot_menus/MainGunTypeSelectMenu.php
+++ src/mine_deep_rock/slot_menus/MainGunTypeSelectMenu.php
@@ -5,7 +5,6 @@ namespace mine_deep_rock\slot_menus;
 
 
 use gun_system\models\GunType;
-use military_department_system\MilitaryDepartmentSystem;
 use military_department_system\models\AssaultSoldier;
 use military_department_system\models\Engineer;
 use military_department_system\models\MilitaryDepartment;
@@ -15,42 +14,43 @@ use pocketmine\item\ItemIds;
 use pocketmine\Player;
 use slot_menu_system\models\SlotMenu;
 use slot_menu_system\models\SlotMenuElement;
+use slot_menu_system\SlotMenuSystem;
 
 class MainGunTypeSelectMenu extends SlotMenu
 {
     public function __construct(MilitaryDepartment $militaryDepartment) {
         $sendGunSelectMenu = function (Player $player, GunType $gunType) {
-            //TODO:実装
+            SlotMenuSystem::send($player,new GunSelectMenu($player->getName(),$gunType));
         };
         $menus = [];
         switch ($militaryDepartment::NAME) {
             case AssaultSoldier::NAME:
                 $menus = [
-                    new SlotMenuElement(ItemIds::BOW, "", function (Player $player) use ($sendGunSelectMenu) {
+                    new SlotMenuElement(ItemIds::BOW, GunType::AssaultRifle()->getTypeText(), function (Player $player) use ($sendGunSelectMenu) {
                         $sendGunSelectMenu($player, GunType::AssaultRifle());
                     }),
-                    new SlotMenuElement(ItemIds::BOW, "", function (Player $player) use ($sendGunSelectMenu) {
+                    new SlotMenuElement(ItemIds::BOW, GunType::Shotgun()->getTypeText(), function (Player $player) use ($sendGunSelectMenu) {
                         $sendGunSelectMenu($player, GunType::Shotgun());
                     }),
                 ];
                 break;
             case NursingSoldier::NAME:
                 $menus = [
-                    new SlotMenuElement(ItemIds::BOW, "", function (Player $player) use ($sendGunSelectMenu) {
+                    new SlotMenuElement(ItemIds::BOW,  GunType::SMG()->getTypeText(), function (Player $player) use ($sendGunSelectMenu) {
                         $sendGunSelectMenu($player, GunType::SMG());
                     }),
                 ];
                 break;
             case Engineer::NAME:
                 $menus = [
-                    new SlotMenuElement(ItemIds::BOW, "", function (Player $player) use ($sendGunSelectMenu) {
+                    new SlotMenuElement(ItemIds::BOW, GunType::LMG()->getTypeText(), function (Player $player) use ($sendGunSelectMenu) {
                         $sendGunSelectMenu($player, GunType::LMG());
                     }),
                 ];
                 break;
             case Scout::NAME:
                 $menus = [
-                    new SlotMenuElement(ItemIds::BOW, "", function (Player $player) use ($sendGunSelectMenu) {
+                    new SlotMenuElement(ItemIds::BOW, GunType::SniperRifle()->getTypeText(), function (Player $player) use ($sendGunSelectMenu) {
                         $sendGunSelectMenu($player, GunType::SniperRifle());
                     }),
                 ];
