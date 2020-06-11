f62072d696fd5d87f2771636a35aee126e974662
suinua <maatinosippo@gmail.com>
2020-06-11 22:03:13 +0900

    update

--- src/mine_deep_rock/listeners/BoxListener.php
+++ src/mine_deep_rock/listeners/BoxListener.php
@@ -8,18 +8,25 @@ use box_system\pmmp\events\AmmoBoxEffectOnEvent;
 use box_system\pmmp\events\FlareBoxEffectOnEvent;
 use box_system\pmmp\events\MedicineBoxEffectOnEvent;
 use gun_system\GunSystem;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
 use pocketmine\event\Listener;
 
 class BoxListener implements Listener
 {
     public function onAmmoBoxEffect(AmmoBoxEffectOnEvent $event): void {
-        GunSystem::giveAmmo($event->getReceiver(), 0, 10);
-        GunSystem::giveAmmo($event->getReceiver(), 1, 10);
+        $receiver = $event->getReceiver();
+        GunSystem::giveAmmo($receiver, 0, 10);
+        GunSystem::giveAmmo($receiver, 1, 10);
         //TODO:メッセージ
     }
+
     public function onMedicineBoxEffect(MedicineBoxEffectOnEvent $event): void {
-        //TODO:実装
+        $receiver = $event->getReceiver();
+        $receiver->addEffect(new EffectInstance(Effect::getEffect(Effect::REGENERATION), 20 * 2, 1));
+        //TODO:メッセージ
     }
+
     public function onFlareBoxEffect(FlareBoxEffectOnEvent $event): void {
         //TODO:実装
     }
