b2fcabb460bc6ff28a3889c22562ade9d4460ee7
suinua <maatinosippo@gmail.com>
2020-06-12 17:05:18 +0900

    弾丸によるダメージと制圧射撃を実装

--- src/mine_deep_rock/listeners/GunListener.php
+++ src/mine_deep_rock/listeners/GunListener.php
@@ -4,13 +4,30 @@
 namespace mine_deep_rock\listeners;
 
 
+use gun_system\GunSystem;
 use gun_system\pmmp\event\BulletHitEvent;
+use gun_system\pmmp\event\BulletHitNearEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
+use team_system\TeamSystem;
 
 class GunListener implements Listener
 {
+    public function onBulletHit(BulletHitEvent $event) {
+        $pk = new EntityDamageByEntityEvent($event->getAttacker(), $event->getVictim(), EntityDamageEvent::CAUSE_CONTACT, $event->getDamage());
+        $pk->call();
+    }
 
-    public function onHitBullet(BulletHitEvent $event) {
+    public function onBulletHitNear(BulletHitNearEvent $event) {
+        $attacker = $event->getAttacker();
+        $victim = $event->getVictim();
 
+        $attackerData = TeamSystem::getPlayerData($attacker);
+        $victimData = TeamSystem::getPlayerData($victim);
+        if ($attackerData->getBelongTeamId() === null || $victimData->getBelongTeamId() === null) return;
+        if ($attackerData->getBelongTeamId()->equal($victimData->getBelongTeamId())) {
+            GunSystem::threaten($victim);
+        }
     }
 }
