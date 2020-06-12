927ac35b4f400daa27ca03bebac209c0c602d489
suinua <maatinosippo@gmail.com>
2020-06-12 16:53:59 +0900

    銃のListenerを追加

---
+++ src/mine_deep_rock/listeners/GunListener.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace mine_deep_rock\listeners;
+
+
+use gun_system\pmmp\event\BulletHitEvent;
+use pocketmine\event\Listener;
+
+class GunListener implements Listener
+{
+
+    public function onHitBullet(BulletHitEvent $event) {
+
+    }
+}
