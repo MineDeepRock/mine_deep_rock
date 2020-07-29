a7dc0d4d5f68e3c0af04e013cfd9a14bfffba9fc
suinua <maatinosippo@gmail.com>
2020-07-29 21:54:10 +0900

    add DataFolderPath class

---
+++ src/mine_deep_rock/DataFolderPath.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace mine_deep_rock;
+
+
+class DataFolderPath
+{
+    const PATH = "./plugin_data/MineDeepRock/";
+    const MilitaryDepartment = self::PATH . "military_departments/";
+}
