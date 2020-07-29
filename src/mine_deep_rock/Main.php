037ceea666e7fd29cf1cdcae3d77c2ec9ee692dd
suinua <maatinosippo@gmail.com>
2020-07-29 22:42:54 +0900

    update

--- src/mine_deep_rock/Main.php
+++ src/mine_deep_rock/Main.php
@@ -2,6 +2,8 @@
 
 namespace mine_deep_rock;
 
+use mine_deep_rock\dao\PlayerStatusDAO;
+use mine_deep_rock\model\PlayerStatus;
 use mine_deep_rock\pmmp\listener\TDMListener;
 use mine_deep_rock\store\MilitaryDepartmentsStore;
 use pocketmine\event\Listener;
@@ -12,13 +14,20 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase implements Listener
 {
     public function onEnable() {
+        PlayerStatusDAO::init();
         MilitaryDepartmentsStore::init();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getPluginManager()->registerEvents(new TDMListener($this->getScheduler()), $this);
     }
 
     public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
         $playerName = $player->getName();
+
+        if (!PlayerStatusDAO::isExist($playerName)) {
+            PlayerStatusDAO::save(new PlayerStatus($playerName, MilitaryDepartmentsStore::get("AssaultSoldier")));
+        }
+
         $pk = new GameRulesChangedPacket();
         $pk->gameRules["doImmediateRespawn"] = [1, true];
         $player->sendDataPacket($pk);
