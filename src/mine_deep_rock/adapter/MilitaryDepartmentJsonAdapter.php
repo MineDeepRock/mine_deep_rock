19e1c19a331ed0de43214d7ab73106964654e60f
suinua <maatinosippo@gmail.com>
2020-07-29 21:40:41 +0900

    add MilitaryDepartmentJsonAdapter

---
+++ src/mine_deep_rock/adapter/MilitaryDepartmentJsonAdapter.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace mine_deep_rock\adapter;
+
+
+use box_system\models\Box;
+use grenade_system\models\Grenade;
+use gun_system\model\GunType;
+use mine_deep_rock\model\MilitaryDepartment;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+
+class MilitaryDepartmentJsonAdapter
+{
+    static function decode(array $json): MilitaryDepartment {
+        $gunTypes = array_map(function (string $name) {
+            return GunType::fromString($name);
+        }, $json["gun_types"]);
+
+        $boxes = array_map(function (string $name) {
+            return Box::fromString($name);
+        }, $json["boxes"]);
+
+        $effectInstances = array_map(function (string $effectInstance) {
+            return new EffectInstance(Effect::getEffect($effectInstance["id"]), $effectInstance["duration"], $effectInstance["amplifier"], $effectInstance["visible"]);
+        }, $json["effect_instances"]);
+
+        $grenades = array_map(function (string $name) {
+            return Grenade::fromString($name);
+        }, $json["grenades"]);
+
+        return new MilitaryDepartment(
+            $json["name"],
+            $gunTypes,
+            $json["default_gun_name"],
+            $boxes,
+            $effectInstances,
+            $grenades
+        );
+    }
+}
