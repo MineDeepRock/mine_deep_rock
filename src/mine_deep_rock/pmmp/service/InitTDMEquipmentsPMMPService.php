0db11ddeb36acfcf96bb00142f8d06f52bc3c80c
suinua <maatinosippo@gmail.com>
2020-08-01 16:48:00 +0900

    InitTDMEquipmentsPMMPServiceの実装

--- src/mine_deep_rock/pmmp/service/InitTDMEquipmentsPMMPService.php
+++ src/mine_deep_rock/pmmp/service/InitTDMEquipmentsPMMPService.php
@@ -4,12 +4,41 @@
 namespace mine_deep_rock\pmmp\service;
 
 
+use box_system\models\Box;
+use box_system\pmmp\items\BoxItem;
+use box_system\pmmp\items\SpawnAmmoBoxItem;
+use grenade_system\pmmp\items\GrenadeItem;
+use gun_system\GunSystem;
+use gun_system\model\attachment\Scope;
+use mine_deep_rock\dao\GunRecordDAO;
+use mine_deep_rock\dao\PlayerStatusDAO;
 use pocketmine\Player;
 
 class InitTDMEquipmentsPMMPService
 {
     static function execute(Player $player): void {
-        //TODO:装備の初期化
         $player->getInventory()->setContents([]);
+
+        $status = PlayerStatusDAO::get($player->getName());
+
+        $mainGunRecord = GunRecordDAO::get($player->getName(), $status->getMainGunName());
+        $mainGun = GunSystem::findGunByName($mainGunRecord->getName());
+        $mainGun->setScope(new Scope($mainGunRecord->getScopeMagnification()));
+
+        $subGunRecord = GunRecordDAO::get($player->getName(), $status->getSubGunName());
+        $subGun = GunSystem::findGunByName($subGunRecord->getName());
+        $subGun->setScope(new Scope($subGunRecord->getScopeMagnification()));
+
+        $inventory = $player->getInventory();
+        $inventory->addItem(
+            GunSystem::getItemGunFromGun($mainGun),
+            GunSystem::getItemGunFromGun($subGun)
+        );
+
+        $boxes = $status->getMilitaryDepartment()->getBoxes();
+        foreach ($boxes as $box) $inventory->addItem(BoxItem::fromBox($box));
+
+        $grenades = $status->getMilitaryDepartment()->getGrenades();
+        foreach ($grenades as $grenade) $inventory->addItem(GrenadeItem::fromGrenade($grenade));
     }
 }
