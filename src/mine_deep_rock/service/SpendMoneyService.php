393c4fdeb97b46887922d8aa2dcd96af9900ebae
suinua <maatinosippo@gmail.com>
2020-08-01 10:42:49 +0900

    add SpendMoneyService

---
+++ src/mine_deep_rock/service/SpendMoneyService.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace mine_deep_rock\service;
+
+
+use mine_deep_rock\dao\PlayerStatusDAO;
+use mine_deep_rock\model\PlayerStatus;
+
+class SpendMoneyService
+{
+    static function execute(string $name, int $amount): void {
+        $status = PlayerStatusDAO::get($name);
+        PlayerStatusDAO::update(new PlayerStatus(
+                $status->getName(),
+                $status->getMilitaryDepartment(),
+                $status->getMainGunName(),
+                $status->getSubGunName(),
+                $status->getMoney() - $amount)
+        );
+    }
+}
