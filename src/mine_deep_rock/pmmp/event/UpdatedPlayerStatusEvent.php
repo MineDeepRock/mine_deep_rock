2a01f29dbdcc807fb399e07b279c31a3100d6923
suinua <maatinosippo@gmail.com>
2020-08-01 18:46:53 +0900

    add UpdatedPlayerStatusEvent

---
+++ src/mine_deep_rock/pmmp/event/UpdatedPlayerStatusEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace mine_deep_rock\pmmp\event;
+
+
+use mine_deep_rock\model\PlayerStatus;
+use pocketmine\event\Event;
+
+class UpdatedPlayerStatusEvent extends Event
+{
+    /**
+     * @var PlayerStatus
+     */
+    private $playerStatus;
+
+    public function __construct(PlayerStatus $playerStatus) {
+        $this->playerStatus = $playerStatus;
+    }
+
+    /**
+     * @return PlayerStatus
+     */
+    public function getPlayerStatus(): PlayerStatus {
+        return $this->playerStatus;
+    }
+}
