cf3264caf411c50ce5c0882cd3381b52f49716fa
suinua <maatinosippo@gmail.com>
2020-08-01 11:41:38 +0900

    rename SpawnOnTDMPMMPService to ResortToTDMPMMPService

--- src/mine_deep_rock/pmmp/service/ResortToTDMPMMPService.php
+++ src/mine_deep_rock/pmmp/service/ResortToTDMPMMPService.php
@@ -4,14 +4,22 @@
 namespace mine_deep_rock\pmmp\service;
 
 
+use pocketmine\level\Position;
 use pocketmine\Player;
 use team_game_system\TeamGameSystem;
 
-class SpawnOnTDMPMMPService
+class ResortToTDMPMMPService
 {
-    static function execute(Player $player): void {
+    static function execute(Player $player, Position $pos = null): void {
         $player->setGamemode(Player::ADVENTURE);
         $player->setImmobile(false);
+
+        if ($pos !== null) {
+            $player->teleport($pos);
+        } else {
+            TeamGameSystem::setSpawnPoint($player);
+            $player->teleport($player->getSpawn());
+        }
 
         RemoveCadaverEntityPMMPService::execute($player);
 
