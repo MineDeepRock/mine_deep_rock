0114112de1a9f40ee72f64327240632248e32489
suinua <maatinosippo@gmail.com>
2020-08-01 11:59:51 +0900

    add BuyGunService

---
+++ src/mine_deep_rock/service/BuyGunService.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace mine_deep_rock\service;
+
+
+use mine_deep_rock\dao\GunRecordDAO;
+use mine_deep_rock\dao\PlayerStatusDAO;
+use mine_deep_rock\model\GunRecord;
+
+class BuyGunService
+{
+    static function execute(string $name, string $gunName) {
+        $status = PlayerStatusDAO::get($name);
+
+        foreach (GunRecordDAO::getOwn($name) as $gunRecord) {
+            if ($gunRecord->getName() === $gunName) {
+                return false;
+            }
+        }
+
+        if ($status->getMoney() <= 2000) {
+            return false;
+        }
+
+        GunRecordDAO::add($name, GunRecord::asNew($gunName));
+        return true;
+    }
+}
