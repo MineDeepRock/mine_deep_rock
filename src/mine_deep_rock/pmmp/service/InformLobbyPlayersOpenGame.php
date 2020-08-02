e37ff63b990d0858df22b2e4c780640e7cff509f
suinua <maatinosippo@gmail.com>
2020-08-02 16:59:11 +0900

    add InformLobbyPlayersOpenGame

---
+++ src/mine_deep_rock/pmmp/service/InformLobbyPlayersOpenGame.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace mine_deep_rock\pmmp\service;
+
+
+use pocketmine\Server;
+
+class InformLobbyPlayersOpenGame
+{
+    static function execute(string $gameType) {
+        $level = Server::getInstance()->getLevel("lobby");
+        foreach ($level->getPlayers() as $player) {
+            $player->sendMessage("{$gameType}が開かれました");
+        }
+    }
+}
