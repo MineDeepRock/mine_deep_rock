7008bd9ebcd75c7b6d3200fb24fe6d7bf525707b
suinua <maatinosippo@gmail.com>
2020-08-03 14:39:24 +0900

    防具を着るように

--- src/mine_deep_rock/pmmp/service/InitTDMEquipmentsPMMPService.php
+++ src/mine_deep_rock/pmmp/service/InitTDMEquipmentsPMMPService.php
@@ -11,7 +11,17 @@ use gun_system\model\attachment\Scope;
 use mine_deep_rock\dao\GunRecordDAO;
 use mine_deep_rock\dao\PlayerStatusDAO;
 use pocketmine\item\Arrow;
+use pocketmine\item\ChainHelmet;
+use pocketmine\item\GoldBoots;
+use pocketmine\item\GoldChestplate;
+use pocketmine\item\GoldHelmet;
+use pocketmine\item\GoldLeggings;
+use pocketmine\item\IronBoots;
+use pocketmine\item\IronChestplate;
+use pocketmine\item\IronHelmet;
+use pocketmine\item\IronLeggings;
 use pocketmine\Player;
+use team_game_system\TeamGameSystem;
 
 class InitTDMEquipmentsPMMPService
 {
@@ -20,6 +30,7 @@ class InitTDMEquipmentsPMMPService
 
         $status = PlayerStatusDAO::get($player->getName());
 
+        //銃のセット
         $mainGunRecord = GunRecordDAO::get($player->getName(), $status->getMainGunName());
         $mainGun = GunSystem::findGunByName($mainGunRecord->getName());
         $mainGun->setScope(new Scope($mainGunRecord->getScopeMagnification()));
@@ -34,12 +45,37 @@ class InitTDMEquipmentsPMMPService
             GunSystem::getItemGunFromGun($subGun)
         );
 
+        //箱のセット
         $boxes = $status->getMilitaryDepartment()->getBoxes();
         foreach ($boxes as $box) $inventory->addItem(BoxItem::fromBox($box));
 
+        //グレネードのセット
         $grenades = $status->getMilitaryDepartment()->getGrenades();
         foreach ($grenades as $grenade) $inventory->addItem(GrenadeItem::fromGrenade($grenade));
 
+        //矢のセット
         $inventory->setItem(8, new Arrow());
+
+        //防具のセット
+        //TODO:リファクタリング
+        $playerData = TeamGameSystem::getPlayerData($player);
+        $game = TeamGameSystem::getGame($playerData->getGameId());
+        $teams = $game->getTeams();;
+        foreach ($teams as $team) {
+            if ($team->getId()->equals($playerData->getTeamId())) {
+                $armorInventory = $player->getArmorInventory();
+                if ($team->getName() === "Red") {
+                    $armorInventory->setHelmet(new GoldHelmet());
+                    $armorInventory->setChestplate(new GoldChestplate());
+                    $armorInventory->setLeggings(new GoldLeggings());
+                    $armorInventory->setBoots(new GoldBoots());
+                } else {
+                    $armorInventory->setHelmet(new IronHelmet());
+                    $armorInventory->setChestplate(new IronChestplate());
+                    $armorInventory->setLeggings(new IronLeggings());
+                    $armorInventory->setBoots(new IronBoots());
+                }
+            }
+        }
     }
 }
