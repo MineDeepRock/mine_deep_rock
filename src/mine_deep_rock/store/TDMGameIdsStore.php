cbc9db770221545edb1843cdf0fd21d782b290b3
suinua <maatinosippo@gmail.com>
2020-08-03 21:36:50 +0900

    add delete method

--- src/mine_deep_rock/store/TDMGameIdsStore.php
+++ src/mine_deep_rock/store/TDMGameIdsStore.php
@@ -13,10 +13,20 @@ class TDMGameIdsStore
      * @var GameId[]
      */
     private static $ids = [];
-    static function add(GameId $gameId) :void {
+
+    static function add(GameId $gameId): void {
         self::$ids[] = $gameId;
     }
-    static function getAll():array{
+
+    static function getAll(): array {
         return self::$ids;
+    }
+
+    static function delete(GameId $gameId): void {
+        foreach (self::$ids as $key => $id) {
+            if ($id->equals($gameId)) {
+                unset(self::$ids[$key]);
+            }
+        }
     }
 }
