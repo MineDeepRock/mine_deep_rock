75bf3b9245ab5e787724506a60120ac473501c89
suinua <maatinosippo@gmail.com>
2020-08-09 12:30:00 +0900

    タスクの消化。エラーを吐くように

--- src/mine_deep_rock/pmmp/service/SetPrivateNameTagPMMPService.php
+++ src/mine_deep_rock/pmmp/service/SetPrivateNameTagPMMPService.php
@@ -4,6 +4,7 @@
 namespace mine_deep_rock\pmmp\service;
 
 
+use LogicException;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 use private_name_tag\models\PrivateNameTag;
@@ -13,8 +14,7 @@ class SetPrivateNameTagPMMPService
     static function execute(Player $player): void {
         $tag = PrivateNameTag::get($player);
         if ($tag !== null) {
-            //TODO : エラーを吐くように
-            return;
+            throw new LogicException("すでにプライベートネームタグがセットされています");
         }
 
         $hpGauge = str_repeat(TextFormat::GREEN . "■", intval($player->getHealth()));
--- src/mine_deep_rock/pmmp/service/UpdatePrivateNameTagPMMPService.php
+++ src/mine_deep_rock/pmmp/service/UpdatePrivateNameTagPMMPService.php
@@ -4,6 +4,7 @@
 namespace mine_deep_rock\pmmp\service;
 
 
+use LogicException;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 use private_name_tag\models\PrivateNameTag;
@@ -13,8 +14,7 @@ class UpdatePrivateNameTagPMMPService
     static function execute(Player $target): void {
         $tag = PrivateNameTag::get($target);
         if ($tag === null) {
-            //TODO : エラーを吐くように
-            return;
+            throw new LogicException("プライベートネームタグがセットされていません");
         }
 
         $hpGauge = str_repeat(TextFormat::GREEN . "■", intval($target->getHealth()));
