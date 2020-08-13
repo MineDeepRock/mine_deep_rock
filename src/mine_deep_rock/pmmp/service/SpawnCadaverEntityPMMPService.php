30b57af5350c1d0b58984f2de0d93bfd0f8a2f0f
suinua <maatinosippo@gmail.com>
2020-08-13 22:09:19 +0900

    死体に蘇生可能かどうか表示されるように

--- src/mine_deep_rock/pmmp/service/SpawnCadaverEntityPMMPService.php
+++ src/mine_deep_rock/pmmp/service/SpawnCadaverEntityPMMPService.php
@@ -5,12 +5,22 @@ namespace mine_deep_rock\pmmp\service;
 
 
 use mine_deep_rock\pmmp\entity\CadaverEntity;
+use mine_deep_rock\store\PlayerGameStatusStore;
 use pocketmine\Player;
 
 class SpawnCadaverEntityPMMPService
 {
     static function execute(Player $victim): void {
+        $victimGameStatus = PlayerGameStatusStore::findByName($victim->getName());
+
+        if ($victimGameStatus->isResuscitated()) {
+            $nameTag = "[蘇生不可能]" . $victim->getName();
+        } else {
+            $nameTag = "[蘇生可能]" . $victim->getName();
+        }
+
         $cadaverEntity = new CadaverEntity($victim->getLevel(), $victim);
+        $cadaverEntity->setNameTag($nameTag);
         $cadaverEntity->spawnToAll();
     }
 }
