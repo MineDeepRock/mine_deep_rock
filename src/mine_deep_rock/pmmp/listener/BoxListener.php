3aa53334a2b1b83c5390d7e36869bc065edd3791
suinua <maatinosippo@gmail.com>
2020-08-13 23:03:14 +0900

    弾薬箱、医療箱はアドベンチャーの味方のみに効くように

--- src/mine_deep_rock/pmmp/listener/BoxListener.php
+++ src/mine_deep_rock/pmmp/listener/BoxListener.php
@@ -44,6 +44,7 @@ class BoxListener implements Listener
     public function onAmmoBoxEffect(AmmoBoxEffectOnEvent $event): void {
         $owner = $event->getOwner();
         $receiver = $event->getReceiver();
+        if ($receiver !== Player::ADVENTURE) return;
 
         $receiverData = TeamGameSystem::getPlayerData($receiver);
         $ownerData = TeamGameSystem::getPlayerData($owner);
@@ -62,6 +63,7 @@ class BoxListener implements Listener
     public function onMedicineBoxEffect(MedicineBoxEffectOnEvent $event): void {
         $receiver = $event->getReceiver();
         $owner = $event->getOwner();
+        if ($receiver !== Player::ADVENTURE) return;
 
         $receiverData = TeamGameSystem::getPlayerData($receiver);
         $ownerData = TeamGameSystem::getPlayerData($owner);
@@ -75,6 +77,8 @@ class BoxListener implements Listener
     public function onFlareBoxEffect(FlareBoxEffectOnEvent $event): void {
         $owner = $event->getOwner();
         $receiver = $event->getReceiver();
+        if ($receiver !== Player::ADVENTURE) return;
+
         $receiverData = TeamGameSystem::getPlayerData($receiver);
         $ownerData = TeamGameSystem::getPlayerData($owner);
 
