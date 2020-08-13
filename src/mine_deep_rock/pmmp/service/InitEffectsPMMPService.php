275390ce44567c596dabd961ee7e91c38db88caa
suinua <maatinosippo@gmail.com>
2020-08-13 22:51:14 +0900

    add InitEffectsPMMPService

---
+++ src/mine_deep_rock/pmmp/service/InitEffectsPMMPService.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace mine_deep_rock\pmmp\service;
+
+
+use mine_deep_rock\dao\PlayerStatusDAO;
+use pocketmine\Player;
+
+class InitEffectsPMMPService
+{
+    static function execute(Player $player): void {
+        $militaryDepartment = PlayerStatusDAO::get($player->getName());
+        foreach ($militaryDepartment->getMilitaryDepartment()->getEffectInstances() as $effectInstance) {
+            $player->addEffect($effectInstance);
+        }
+    }
+}
