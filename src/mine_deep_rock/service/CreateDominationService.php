a3d6b6eeacebaeb8fad4d4d293201f99dad623a3
suinua <maatinosippo@gmail.com>
2020-08-14 23:39:19 +0900

    add CreateDominationService

---
+++ src/mine_deep_rock/service/CreateDominationService.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace mine_deep_rock\service;
+
+
+use mine_deep_rock\store\DominationGameIdsStore;
+use mine_deep_rock\store\TDMGameIdsStore;
+use pocketmine\utils\TextFormat;
+use team_game_system\model\Game;
+use team_game_system\model\Score;
+use team_game_system\model\Team;
+use team_game_system\TeamGameSystem;
+
+class CreateDominationService
+{
+
+    static function execute(?Score $maxScore = null, ?int $maxPlayersCount = null, ?int $timeLimit = null): void {
+        $teams = [
+            Team::asNew("Red", TextFormat::RED),
+            Team::asNew("Blue", TextFormat::BLUE),
+        ];
+        $map = TeamGameSystem::selectMap("BrokenCity", $teams);
+        $game = Game::asNew($map, $teams, $maxScore, $maxPlayersCount, $timeLimit);
+
+        TeamGameSystem::registerGame($game);
+        DominationGameIdsStore::add($game->getId());
+    }
+}
