148169e5e848604d968dea9da39f040619033406
suinua <maatinosippo@gmail.com>
2020-08-14 23:35:08 +0900

    武器種ごとのアイコンが表示されるように

--- src/mine_deep_rock/pmmp/form/GunTypeListForSaleForm.php
+++ src/mine_deep_rock/pmmp/form/GunTypeListForSaleForm.php
@@ -5,6 +5,8 @@ namespace mine_deep_rock\pmmp\form;
 
 
 use form_builder\models\simple_form_elements\SimpleFormButton;
+use form_builder\models\simple_form_elements\SimpleFormImage;
+use form_builder\models\simple_form_elements\SimpleFormImageType;
 use form_builder\models\SimpleForm;
 use gun_system\model\GunType;
 use pocketmine\Player;
@@ -16,56 +18,80 @@ class GunTypeListForSaleForm extends SimpleForm
         $buttons = [
             new SimpleFormButton(
                 GunType::AssaultRifle()->getTypeText(),
-                null,
+                new SimpleFormImage(
+                    SimpleFormImageType::Path(),
+                    "textures/icon/" . GunType::AssaultRifle()->getTypeText()
+                ),
                 function (Player $player) use ($taskScheduler) {
                     $player->sendForm(new GunForSaleListForm($player, GunType::AssaultRifle(), $taskScheduler));
                 }
             ),
             new SimpleFormButton(
                 GunType::Shotgun()->getTypeText(),
-                null,
+                new SimpleFormImage(
+                    SimpleFormImageType::Path(),
+                    "textures/icon/" . GunType::Shotgun()->getTypeText()
+                ),
                 function (Player $player) use ($taskScheduler) {
                     $player->sendForm(new GunForSaleListForm($player, GunType::Shotgun(), $taskScheduler));
                 }
             ),
             new SimpleFormButton(
                 GunType::SMG()->getTypeText(),
-                null,
+                new SimpleFormImage(
+                    SimpleFormImageType::Path(),
+                    "textures/icon/" . GunType::SMG()->getTypeText()
+                ),
                 function (Player $player) use ($taskScheduler) {
                     $player->sendForm(new GunForSaleListForm($player, GunType::SMG(), $taskScheduler));
                 }
             ),
             new SimpleFormButton(
                 GunType::LMG()->getTypeText(),
-                null,
+                new SimpleFormImage(
+                    SimpleFormImageType::Path(),
+                    "textures/icon/" . GunType::LMG()->getTypeText()
+                ),
                 function (Player $player) use ($taskScheduler) {
                     $player->sendForm(new GunForSaleListForm($player, GunType::LMG(), $taskScheduler));
                 }
             ),
             new SimpleFormButton(
                 GunType::SniperRifle()->getTypeText(),
-                null,
+                new SimpleFormImage(
+                    SimpleFormImageType::Path(),
+                    "textures/icon/" . GunType::SniperRifle()->getTypeText()
+                ),
                 function (Player $player) use ($taskScheduler) {
                     $player->sendForm(new GunForSaleListForm($player, GunType::SniperRifle(), $taskScheduler));
                 }
             ),
             new SimpleFormButton(
                 GunType::DMR()->getTypeText(),
-                null,
+                new SimpleFormImage(
+                    SimpleFormImageType::Path(),
+                    "textures/icon/" . GunType::DMR()->getTypeText()
+                ),
                 function (Player $player) use ($taskScheduler) {
                     $player->sendForm(new GunForSaleListForm($player, GunType::DMR(), $taskScheduler));
                 }
             ),
             new SimpleFormButton(
                 GunType::HandGun()->getTypeText(),
-                null,
+                new SimpleFormImage(
+                    SimpleFormImageType::Path(),
+                    "textures/icon/" . GunType::HandGun()->getTypeText()
+                ),
                 function (Player $player) use ($taskScheduler) {
                     $player->sendForm(new GunForSaleListForm($player, GunType::HandGun(), $taskScheduler));
                 }
             ),
             new SimpleFormButton(
                 GunType::Revolver()->getTypeText(),
-                null,
+                new SimpleFormImage(
+                    SimpleFormImageType::Path(),
+                    "textures/icon/" . GunType::Revolver()->getTypeText()
+                ),
                 function (Player $player) use ($taskScheduler) {
                     $player->sendForm(new GunForSaleListForm($player, GunType::Revolver(), $taskScheduler));
                 }
