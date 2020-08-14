4c3eaab12a653db677f6a3f1a65f3bf475420bdd
suinua <maatinosippo@gmail.com>
2020-08-14 23:38:25 +0900

    add DominationGameIdsStore

---
+++ src/mine_deep_rock/store/DominationGameIdsStore.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace mine_deep_rock\store;
+
+
+use team_game_system\model\GameId;
+
+class DominationGameIdsStore
+{
+
+    /**
+     * @var GameId[]
+     */
+    private static $ids = [];
+
+    static function add(GameId $gameId): void {
+        self::$ids[] = $gameId;
+    }
+
+    static function getAll(): array {
+        return self::$ids;
+    }
+
+    static function delete(GameId $gameId): void {
+        foreach (self::$ids as $key => $id) {
+            if ($id->equals($gameId)) {
+                unset(self::$ids[$key]);
+            }
+        }
+
+        self::$ids = array_values(self::$ids);
+    }
+}
