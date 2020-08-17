28a106c76b6c639f00302c0871a770cba47f6eab
suinua <maatinosippo@gmail.com>
2020-08-17 15:15:43 +0900

    add  path

--- src/mine_deep_rock/DataFolderPath.php
+++ src/mine_deep_rock/DataFolderPath.php
@@ -12,4 +12,5 @@ class DataFolderPath
     const GunRecord = self::PATH . "gun_record/";
     const Geometry = self::PATH . "geometry/";
     const Skin = self::PATH . "skin/";
+    const DominationFlagData = self::PATH . "domination_flag_data/";
 }
