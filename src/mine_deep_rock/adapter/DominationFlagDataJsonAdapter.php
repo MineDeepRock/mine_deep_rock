809f5fce072842cba57c8a88d5decaaa7fdc16b3
suinua <maatinosippo@gmail.com>
2020-08-17 15:15:32 +0900

    add DominationFlagDataJsonAdapter

---
+++ src/mine_deep_rock/adapter/DominationFlagDataJsonAdapter.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace mine_deep_rock\adapter;
+
+
+use mine_deep_rock\model\DominationFlagData;
+use mine_deep_rock\model\PlayerStatus;
+use mine_deep_rock\store\MilitaryDepartmentsStore;
+use pocketmine\level\Position;
+
+class DominationFlagDataJsonAdapter
+{
+    static function encode(DominationFlagData $flagData): array {
+        $pos = $flagData->getPosition();
+        return [
+            "name" => $flagData->getName(),
+            "position" => [
+                "x" => $pos->getX(),
+                "y" => $pos->getY(),
+                "z" => $pos->getZ(),
+            ]
+        ];
+    }
+
+    static function decode(array $json): DominationFlagData {
+        $pos = $json["position"];
+        return new DominationFlagData(
+            $json["name"],
+            new Position(
+                $pos["x"],
+                $pos["y"],
+                $pos["z"]
+            )
+        );
+    }
+}
