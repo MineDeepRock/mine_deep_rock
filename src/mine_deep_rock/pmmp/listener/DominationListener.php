7418b852f6349d20df8e23293dba0d5f8b58d0cd
suinua <maatinosippo@gmail.com>
2020-08-17 12:57:08 +0900

    add DominationListener

---
+++ src/mine_deep_rock/pmmp/listener/DominationListener.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace mine_deep_rock\pmmp\listener;
+
+
+use mine_deep_rock\pmmp\scoreboard\DominationScoreboard;
+use mine_deep_rock\service\OccupyFlagService;
+use mine_deep_rock\store\DominationFlagsStore;
+use pocketmine\Server;
+use team_game_system\pmmp\event\UpdatedGameTimerEvent;
+use team_game_system\TeamGameSystem;
+
+class DominationListener
+{
+    public function onUpdatedTime(UpdatedGameTimerEvent $event): void {
+        $gameId = $event->getGameId();
+        $game = TeamGameSystem::getGame($gameId);
+
+        $flags = DominationFlagsStore::findByGameId($gameId);
+        $levelPlayers = Server::getInstance()->getLevelByName($game->getMap()->getName())->getPlayers();
+
+
+        foreach ($flags as $flag) {
+            $aroundPlayersData = [];
+            foreach ($levelPlayers as $player) {
+                if (!$player->isOnline()) continue;
+                if ($player->getPosition()->distance($flag->getPosition()) <= 10) {
+                    $aroundPlayersData[] = TeamGameSystem::getPlayerData($player);
+                }
+            }
+
+            OccupyFlagService::execute($flag, $aroundPlayersData);
+        }
+
+        $server = Server::getInstance();
+        foreach (TeamGameSystem::getGamePlayersData($gameId) as $playerData) {
+            $player = $server->getPlayer($playerData->getName());
+
+            DominationScoreboard::update(
+                $player,
+                $game,
+                DominationFlagsStore::findByGameId($gameId));
+        }
+    }
+}
