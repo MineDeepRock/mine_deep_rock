91c01f88d8ce35c2cfea607036098544df1e0af1
suinua <maatinosippo@gmail.com>
2020-08-17 17:09:04 +0900

    update

--- src/mine_deep_rock/pmmp/listener/DominationListener.php
+++ src/mine_deep_rock/pmmp/listener/DominationListener.php
@@ -6,9 +6,11 @@ namespace mine_deep_rock\pmmp\listener;
 
 use mine_deep_rock\GameTypeList;
 use mine_deep_rock\pmmp\scoreboard\DominationScoreboard;
+use mine_deep_rock\pmmp\service\SummonFlagParticlePMMPService;
 use mine_deep_rock\service\OccupyFlagService;
 use mine_deep_rock\store\DominationFlagsStore;
 use pocketmine\Server;
+use team_game_system\model\Score;
 use team_game_system\pmmp\event\UpdatedGameTimerEvent;
 use team_game_system\TeamGameSystem;
 
@@ -17,10 +19,11 @@ class DominationListener
     public function onUpdatedTime(UpdatedGameTimerEvent $event): void {
         $gameId = $event->getGameId();
         $game = TeamGameSystem::getGame($gameId);
+        $level = Server::getInstance()->getLevelByName($game->getMap()->getLevelName());
         if (!$game->getType()->equals(GameTypeList::Domination())) return;
 
         $flags = DominationFlagsStore::findByGameId($gameId);
-        $levelPlayers = Server::getInstance()->getLevelByName($game->getMap()->getName())->getPlayers();
+        $levelPlayers = $level->getPlayers();
 
 
         foreach ($flags as $flag) {
@@ -36,8 +39,10 @@ class DominationListener
 
             //TODO:$flagが更新されるか確認
             if ($flag->getGauge()->isOccupied()) {
-                TeamGameSystem::addScore($flag->getGameId(), $flag->getGauge()->getOccupyingTeamId(), 1);
+                TeamGameSystem::addScore($flag->getGameId(), $flag->getGauge()->getOccupyingTeamId(), new Score(1));
             }
+
+            SummonFlagParticlePMMPService::execute($flag, $level);
         }
 
         $server = Server::getInstance();
