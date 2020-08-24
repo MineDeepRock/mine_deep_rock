b1bd5d580ad0a9f76ef14d31396b1941c8037212
suinua <maatinosippo@gmail.com>
2020-08-24 15:42:23 +0900

    fix

--- src/mine_deep_rock/service/SendOneOnOneRequestService.php
+++ src/mine_deep_rock/service/SendOneOnOneRequestService.php
@@ -6,12 +6,13 @@ namespace mine_deep_rock\service;
 
 use mine_deep_rock\model\OneOnOneRequest;
 use mine_deep_rock\store\OneOnOneRequestsStore;
+use team_game_system\model\Score;
 
 class SendOneOnOneRequestService
 {
-    static function execute(string $ownerName, string $receiverName): bool {
+    static function execute(string $ownerName, string $receiverName, string $mapName, ?Score $maxScore, int $timeLimit = 600): bool {
         //知識:リクエストは一つしか送れない
-        $request = OneOnOneRequest::create($ownerName, $receiverName);
+        $request = OneOnOneRequest::create($ownerName, $receiverName, $mapName, $maxScore, $timeLimit);
         if (OneOnOneRequestsStore::findByOwnerName($request->getOwnerName()) !== null) return false;
 
         OneOnOneRequestsStore::add($request);
