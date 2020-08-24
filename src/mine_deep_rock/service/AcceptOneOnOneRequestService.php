fe2366abf5ba766b982af1b28eda6f6d28acc19e
suinua <maatinosippo@gmail.com>
2020-08-24 15:23:57 +0900

    add AcceptOneOnOneRequestService

---
+++ src/mine_deep_rock/service/AcceptOneOnOneRequestService.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace mine_deep_rock\service;
+
+
+use mine_deep_rock\GameTypeList;
+use mine_deep_rock\model\OneOnOneRequest;
+use mine_deep_rock\store\OneOnOneRequestsStore;
+use pocketmine\utils\TextFormat;
+use team_game_system\model\Game;
+use team_game_system\model\Team;
+use team_game_system\TeamGameSystem;
+
+class AcceptOneOnOneRequestService
+{
+    static function execute(OneOnOneRequest $request): void {
+        OneOnOneRequestsStore::delete($request);
+
+        $teams = [
+            Team::asNew($request->getOwnerName(), TextFormat::RED),
+            Team::asNew($request->getReceiverName(), TextFormat::BLUE),
+        ];
+        $map = TeamGameSystem::selectMap($request->getMapName(), $teams);
+        $game = Game::asNew(GameTypeList::TDM(), $map, $teams, $request->getMaxScore(), 2, $request->getTimeLimit());
+
+        TeamGameSystem::registerGame($game);
+    }
+}
