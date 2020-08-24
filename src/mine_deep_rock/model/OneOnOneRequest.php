3667371a57faad569d2f1b05e122aec28daf6325
suinua <maatinosippo@gmail.com>
2020-08-24 15:22:42 +0900

    update

--- src/mine_deep_rock/model/OneOnOneRequest.php
+++ src/mine_deep_rock/model/OneOnOneRequest.php
@@ -4,19 +4,39 @@
 namespace mine_deep_rock\model;
 
 
+use team_game_system\model\Score;
+
 class OneOnOneRequest
 {
     private $ownerName;
     private $receiverName;
 
-    public function __construct(string $ownerName, string $receiverName) {
+    /**
+     * @var string
+     */
+    private $mapName;
+    /**
+     * @var Score
+     */
+    private $maxScore;
+    /**
+     * @var int
+     */
+    private $timeLimit;
+
+    function __construct(string $ownerName, string $receiverName, string $mapName, ?Score $maxScore, int $timeLimit) {
         $this->ownerName = $ownerName;
         $this->receiverName = $receiverName;
+        $this->mapName = $mapName;
+        $this->maxScore = $maxScore;
+        $this->timeLimit = $timeLimit;
     }
 
-    static function create(string $ownerName, string $receiverName): ?OneOnOneRequest {
+    static function create(string $ownerName, string $receiverName, string $mapName, ?Score $maxScore, int $timeLimit = 600): ?OneOnOneRequest {
         if ($ownerName === $receiverName) return null;
-        return new OneOnOneRequest($ownerName, $receiverName);
+
+        if ($timeLimit > 600) $timeLimit = 600;
+        return new OneOnOneRequest($ownerName, $receiverName, $mapName, $maxScore, $timeLimit);
     }
 
     /**
@@ -31,5 +51,26 @@ class OneOnOneRequest
      */
     public function getReceiverName(): string {
         return $this->receiverName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMapName(): string {
+        return $this->mapName;
+    }
+
+    /**
+     * @return null|Score
+     */
+    public function getMaxScore(): ?Score {
+        return $this->maxScore;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimeLimit(): int {
+        return $this->timeLimit;
     }
 }
