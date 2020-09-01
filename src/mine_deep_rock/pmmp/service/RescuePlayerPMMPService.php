2f693150528383cbc1842afaa28df11d437741fa
suinua <maatinosippo@gmail.com>
2020-09-01 21:26:19 +0900

    二度目の正直を実装

--- src/mine_deep_rock/pmmp/service/RescuePlayerPMMPService.php
+++ src/mine_deep_rock/pmmp/service/RescuePlayerPMMPService.php
@@ -6,8 +6,11 @@ namespace mine_deep_rock\pmmp\service;
 
 use mine_deep_rock\dao\PlayerStatusDAO;
 use mine_deep_rock\model\MilitaryDepartment;
+use mine_deep_rock\model\skill\assault_soldier\SecondChance;
 use mine_deep_rock\service\UpdatePlayerGameStatusIsResuscitated;
 use mine_deep_rock\store\PlayerGameStatusStore;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
 use pocketmine\Player;
 use team_game_system\TeamGameSystem;
 
@@ -26,6 +29,11 @@ class RescuePlayerPMMPService
             if ($playerStatus->getMilitaryDepartment()->getName() === MilitaryDepartment::NursingSoldier) {
                 UpdatePlayerGameStatusIsResuscitated::execute($target->getName());
                 ResortPMMPService::execute($target, $target->getPosition(), true);
+
+                $targetStatus = PlayerStatusDAO::get($target->getName());
+                if ($targetStatus->isSelectedSkill(new SecondChance())) {
+                    $target->addEffect(new EffectInstance(Effect::getEffect(Effect::SPEED), null, 1, false));
+                }
 
                 $player->sendMessage($target->getName() . "を蘇生した");
                 $target->sendMessage($player->getName() . "に蘇生されました");
