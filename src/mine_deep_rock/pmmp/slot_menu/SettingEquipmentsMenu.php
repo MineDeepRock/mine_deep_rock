f1cc3d26ba8190faa45f7ed75a1d06a1019d5355
suinua <maatinosippo@gmail.com>
2020-09-01 23:49:43 +0900

    update

--- src/mine_deep_rock/pmmp/slot_menu/SettingEquipmentsMenu.php
+++ src/mine_deep_rock/pmmp/slot_menu/SettingEquipmentsMenu.php
@@ -4,6 +4,7 @@
 namespace mine_deep_rock\pmmp\slot_menu;
 
 
+use mine_deep_rock\pmmp\form\SelectSkillsForm;
 use pocketmine\item\ItemIds;
 use pocketmine\Player;
 use pocketmine\scheduler\TaskScheduler;
@@ -18,13 +19,16 @@ class SettingEquipmentsMenu extends SlotMenu
             new SlotMenuElement(ItemIds::COMPASS, "兵科", 0, function (Player $player) use ($taskScheduler) {
                 SlotMenuSystem::send($player, new SelectMilitaryDepartmentMenu($this));
             }),
-            new SlotMenuElement(ItemIds::DIAMOND_SWORD, "メインウェポン", 1, function (Player $player) use ($taskScheduler) {
+            new SlotMenuElement(ItemIds::WOODEN_SWORD, "専門技能", 1, function (Player $player) use ($taskScheduler) {
+                $player->sendForm(new SelectSkillsForm($player));
+            }),
+            new SlotMenuElement(ItemIds::DIAMOND_SWORD, "メインウェポン", 2, function (Player $player) use ($taskScheduler) {
                 SlotMenuSystem::send($player, new SelectMainGunTypeMenu($player, $this, $taskScheduler));
             }),
-            new SlotMenuElement(ItemIds::IRON_SWORD, "サブウェポン", 2, function (Player $player) use ($taskScheduler) {
+            new SlotMenuElement(ItemIds::IRON_SWORD, "サブウェポン", 3, function (Player $player) use ($taskScheduler) {
                 SlotMenuSystem::send($player, new SelectSubGunTypeMenu($this, $taskScheduler));
             }),
-            new SlotMenuElement(ItemIds::WOODEN_SWORD, "試し打ち", 3, function (Player $player) use ($taskScheduler) {
+            new SlotMenuElement(ItemIds::WOODEN_SWORD, "試し打ち", 4, function (Player $player) use ($taskScheduler) {
                 SlotMenuSystem::send($player, new SelectTrialGunTypesMenu($taskScheduler));
             }),
             //new SlotMenuElement(ItemIds::GOLDEN_SWORD, "銃購入", 4, function (Player $player) use ($taskScheduler) {
--- src/mine_deep_rock/pmmp/slot_menu/SettingEquipmentsOnGameMenu.php
+++ src/mine_deep_rock/pmmp/slot_menu/SettingEquipmentsOnGameMenu.php
@@ -4,6 +4,7 @@
 namespace mine_deep_rock\pmmp\slot_menu;
 
 
+use mine_deep_rock\pmmp\form\SelectSkillsForm;
 use mine_deep_rock\pmmp\service\ResortPMMPService;
 use pocketmine\item\ItemIds;
 use pocketmine\Player;
@@ -19,10 +20,13 @@ class SettingEquipmentsOnGameMenu extends SlotMenu
             new SlotMenuElement(ItemIds::COMPASS, "兵科", 0, function (Player $player) use ($taskScheduler) {
                 SlotMenuSystem::send($player, new SelectMilitaryDepartmentMenu($this));
             }),
-            new SlotMenuElement(ItemIds::DIAMOND_SWORD, "メインウェポン", 1, function (Player $player) use ($taskScheduler) {
+            new SlotMenuElement(ItemIds::WOODEN_SWORD, "専門技能", 1, function (Player $player) use ($taskScheduler) {
+                $player->sendForm(new SelectSkillsForm($player));
+            }),
+            new SlotMenuElement(ItemIds::DIAMOND_SWORD, "メインウェポン", 2, function (Player $player) use ($taskScheduler) {
                 SlotMenuSystem::send($player, new SelectMainGunTypeMenu($player, $this, $taskScheduler));
             }),
-            new SlotMenuElement(ItemIds::IRON_SWORD, "サブウェポン", 2, function (Player $player) use ($taskScheduler) {
+            new SlotMenuElement(ItemIds::IRON_SWORD, "サブウェポン", 3, function (Player $player) use ($taskScheduler) {
                 SlotMenuSystem::send($player, new SelectSubGunTypeMenu($this, $taskScheduler));
             }),
             new SlotMenuElement(ItemIds::EMERALD, "再出撃", 8, function (Player $player) {
