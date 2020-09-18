e80b2d4a048606b38a1446bcfafef9dbae01c5ae
suinua <maatinosippo@gmail.com>
2020-09-18 19:18:33 +0900

    update

--- src/mine_deep_rock/pmmp/listener/CorePvPListener.php
+++ src/mine_deep_rock/pmmp/listener/CorePvPListener.php
@@ -4,7 +4,12 @@
 namespace mine_deep_rock\pmmp\listener;
 
 
+use mine_deep_rock\store\CoresStore;
+use team_game_system\pmmp\event\FinishedGameEvent;
+
 class CorePvPListener
 {
-
+    public function onFinishedGame(FinishedGameEvent $event) {
+        CoresStore::delete($event->getGame()->getId());
+    }
 }
