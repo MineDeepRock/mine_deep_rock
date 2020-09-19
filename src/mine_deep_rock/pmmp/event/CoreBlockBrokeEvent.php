5f837ec7a96d39abc3a6f4a8ccbe7e48e79f4bc0
suinua <maatinosippo@gmail.com>
2020-09-19 16:40:54 +0900

    add event

---
+++ src/mine_deep_rock/pmmp/event/CoreBlockBrokeEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace mine_deep_rock\pmmp\event;
+
+
+use pocketmine\event\Event;
+use team_game_system\model\GameId;
+use team_game_system\model\TeamId;
+
+class CoreBlockBrokeEvent extends Event
+{
+    private $gameId;
+    private $teamId;
+
+    public function __construct(GameId $gameId,TeamId $teamId) {
+        $this->gameId = $gameId;
+        $this->teamId = $teamId;
+    }
+
+    /**
+     * @return GameId
+     */
+    public function getGameId(): GameId {
+        return $this->gameId;
+    }
+
+    /**
+     * @return TeamId
+     */
+    public function getTeamId(): TeamId {
+        return $this->teamId;
+    }
+}
