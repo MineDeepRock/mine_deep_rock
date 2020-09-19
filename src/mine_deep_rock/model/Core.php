47d3522f08acd805848ac25bd1a84bf5071e0288
suinua <maatinosippo@gmail.com>
2020-09-19 16:41:39 +0900

    update

--- src/mine_deep_rock/model/Core.php
+++ src/mine_deep_rock/model/Core.php
@@ -4,6 +4,8 @@
 namespace mine_deep_rock\model;
 
 
+use mine_deep_rock\pmmp\event\CoreBlockBrokeEvent;
+use mine_deep_rock\pmmp\event\CoreBrokeEvent;
 use pocketmine\level\Position;
 use team_game_system\model\GameId;
 use team_game_system\model\TeamId;
@@ -27,11 +29,18 @@ class Core
      */
     private $position;
 
+
+    /**
+     * @var int
+     */
+    private $blockHealth;
+
     public function __construct(GameId $gameId, TeamId $teamId, Position $position, int $health = 100) {
         $this->gameId = $gameId;
         $this->teamId = $teamId;
         $this->position = $position;
         $this->health = $health;
+        $this->blockHealth = 100;
     }
 
     /**
@@ -59,7 +68,14 @@ class Core
      * @param int $health
      */
     public function setHealth(int $health): void {
+        if ($this->health <= 0) return;
+
         $this->health = $health;
+        if ($this->health <= 0) {
+            $this->health = 0;
+            $event = new CoreBrokeEvent($this->gameId, $this->teamId);
+            $event->call();
+        }
     }
 
     /**
@@ -67,5 +83,21 @@ class Core
      */
     public function getPosition(): Position {
         return $this->position;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBlockHealth(): int {
+        return $this->blockHealth;
+    }
+
+    public function attackCoreBlock(int $damage): void {
+        $this->blockHealth -= $damage;
+        if ($this->blockHealth <= 0) {
+            $this->blockHealth = 100;
+            $event = new CoreBlockBrokeEvent($this->gameId, $this->teamId);
+            $event->call();
+        }
     }
 }
